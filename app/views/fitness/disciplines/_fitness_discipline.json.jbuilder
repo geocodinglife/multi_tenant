7ffecae85acde52252c9cd6d9e076c4973f67afa
Ricardo Alarcon <geocodinglife@gmail.com>
2020-09-18 14:12:54 -0500

    it works

---
+++ app/views/fitness/disciplines/_fitness_discipline.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! fitness_discipline, :id, :name, :enable, :created_at, :updated_at
+json.url fitness_discipline_url(fitness_discipline, format: :json)
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.host = nil
+  config.hosts << "managua.lvh.me"
+  config.hosts << "public.lvh.me"
 end
